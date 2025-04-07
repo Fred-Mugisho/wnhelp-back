1e864204be11d3aa3ef2c4b2e52ff4108d9e2797
Fred-Mugisho <frederickmugisho228@gmail.com>
2025-04-07 10:34:00 +0100

    Reconfig redis
    Signed-off-by: Fred-Mugisho <frederickmugisho228@gmail.com>

--- wnhelp_back/celery.py
+++ wnhelp_back/celery.py
@@ -6,7 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wnhelp_back.settings')
 app = Celery('wnhelp_back')
 
 # Utilise le Redis du serveur
-app.conf.broker_url = 'redis://:TON_MOT_DE_PASSE@127.0.0.1:PORT/0'
+# app.conf.broker_url = 'redis://:TON_MOT_DE_PASSE@127.0.0.1:PORT/0'
 
 # Charge les tâches depuis toutes les apps installées
 app.config_from_object('django.conf:settings', namespace='CELERY')
